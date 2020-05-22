b1c0b5c683f59f983cbbfc6d401452337e5695e1
dengxu <12345678>
2020-05-22 09:25:55 +0800

    监控程序
    新项目

---
+++ src/main/java/com/cn/monitoring/computer/ComputerApplication.java
@@ -0,0 +1,13 @@
+package com.cn.monitoring.computer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerApplication.class, args);
+    }
+
+}
